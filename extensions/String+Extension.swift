2346e25c85dea2b2ada07fcb5131a6f6826384d7
todoroki <s18.todoroki+git@gmail.com>
2018-01-21 18:55:34 +0900

    add trim white space extensions

---
+++ extensions/String+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  String+Extension.swift
+//  extensions
+//
+//  Created by shota_todoroki on 2018/01/21.
+//  Copyright © 2018年 shota_todoroki. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+
+    func trimmedWhiteSpace() -> String {
+        return self.trimmingCharacters(in: .whitespaces)
+    }
+
+    func hasOnlyWhiteSpace() -> Bool {
+        return self.trimmedWhiteSpace() == ""
+    }
+
+}
---
+++ extensionsTests/String+ExtensionTests.swift
@@ -0,0 +1,66 @@
+//
+//  String+ExtensionTests.swift
+//  extensionsTests
+//
+//  Created by shota_todoroki on 2018/01/21.
+//  Copyright © 2018年 shota_todoroki. All rights reserved.
+//
+
+import XCTest
+
+class StringExtensionTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testTrimmedWhiteSpace() {
+        var string, expectedString: String
+
+        string = "hogehoge  "
+        expectedString = "hogehoge"
+        XCTAssertEqual(string.trimmedWhiteSpace(), expectedString)
+
+        string = "  hogehoge"
+        expectedString = "hogehoge"
+        XCTAssertEqual(string.trimmedWhiteSpace(), expectedString)
+
+        string = "  hogehoge  "
+        expectedString = "hogehoge"
+        XCTAssertEqual(string.trimmedWhiteSpace(), expectedString)
+
+        string = "   hoge fuga  "
+        expectedString = "hoge fuga"
+        XCTAssertEqual(string.trimmedWhiteSpace(), expectedString)
+
+        string = "  "
+        expectedString = ""
+        XCTAssertEqual(string.trimmedWhiteSpace(), expectedString)
+    }
+
+    func testHasOnlyWhiteSpace() {
+        var string, expectedString: String
+
+        string = "hogehoge  "
+        XCTAssertFalse(string.hasOnlyWhiteSpace())
+
+        string = "  hogehoge"
+        XCTAssertFalse(string.hasOnlyWhiteSpace())
+
+        string = "  hogehoge  "
+        XCTAssertFalse(string.hasOnlyWhiteSpace())
+
+        string = "   hoge fuga  "
+        XCTAssertFalse(string.hasOnlyWhiteSpace())
+
+        string = "  "
+        XCTAssertTrue(string.hasOnlyWhiteSpace())
+    }
+
+}
