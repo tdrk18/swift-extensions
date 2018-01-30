d7dd923b71a5b2f6015f56ad1d80faaac53a6e12
todoroki <s18.todoroki+git@gmail.com>
2018-01-30 23:53:55 +0900

    use default value

--- extensions/UIColor+Extension.swift
+++ extensions/UIColor+Extension.swift
@@ -10,16 +10,7 @@ import UIKit
 
 public extension UIColor {
 
-    class func rgb(red: Int, green: Int, blue: Int) -> UIColor {
-        return UIColor.init(
-            red: CGFloat(red) / 255.0,
-            green: CGFloat(green) / 255.0,
-            blue: CGFloat(blue) / 255.0,
-            alpha: 1.0
-        )
-    }
-
-    class func rgb(red: Int, green: Int, blue: Int, alpha: CGFloat) -> UIColor {
+    class func rgb(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) -> UIColor {
         return UIColor.init(
             red: CGFloat(red) / 255.0,
             green: CGFloat(green) / 255.0,
@@ -28,15 +19,7 @@ public extension UIColor {
         )
     }
 
-    class func hex(_ hex: String) -> UIColor {
-        let v: [String] = hex.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
-        let r: CGFloat = CGFloat(Int(v[0] + v[1], radix: 16) ?? 0) / 255.0
-        let g: CGFloat = CGFloat(Int(v[2] + v[3], radix: 16) ?? 0) / 255.0
-        let b: CGFloat = CGFloat(Int(v[4] + v[5], radix: 16) ?? 0) / 255.0
-        return UIColor.init(red: r, green: g, blue: b, alpha: 1.0)
-    }
-
-    class func hex(_ hex: String, alpha: CGFloat) -> UIColor {
+    class func hex(_ hex: String, alpha: CGFloat = 1.0) -> UIColor {
         let v: [String] = hex.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
         let r: CGFloat = CGFloat(Int(v[0] + v[1], radix: 16) ?? 0) / 255.0
         let g: CGFloat = CGFloat(Int(v[2] + v[3], radix: 16) ?? 0) / 255.0
