fb5bbff1bd7d600d832f6231d787c41d79e38210
todoroki <s18.todoroki+git@gmail.com>
2018-01-31 00:15:04 +0900

    hex string with "#"

--- extensions/UIColor+Extension.swift
+++ extensions/UIColor+Extension.swift
@@ -20,7 +20,8 @@ public extension UIColor {
     }
 
     class func hexString(_ hex: String, alpha: CGFloat = 1.0) -> UIColor {
-        let v: [String] = hex.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
+        let hexString: String = hex.replacingOccurrences(of: "#", with: "")
+        let v: [String] = hexString.map { String($0) } + Array(repeating: "0", count: max(6 - hexString.count, 0))
         let r: CGFloat = CGFloat(Int(v[0] + v[1], radix: 16) ?? 0) / 255.0
         let g: CGFloat = CGFloat(Int(v[2] + v[3], radix: 16) ?? 0) / 255.0
         let b: CGFloat = CGFloat(Int(v[4] + v[5], radix: 16) ?? 0) / 255.0
