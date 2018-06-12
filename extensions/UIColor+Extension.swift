14c283155065fa53786a63170d4e801d3a20b574
todoroki <s18.todoroki+git@gmail.com>
2018-06-12 23:18:59 +0900

    rename variables

--- extensions/UIColor+Extension.swift
+++ extensions/UIColor+Extension.swift
@@ -21,19 +21,19 @@ public extension UIColor {
 
     class func hex(_ hex: UInt32, alpha: CGFloat = 1.0) -> UIColor {
         let hexString: String = String(hex, radix: 16)
-        let value: [String] = Array(repeating: "0", count: max(6 - hexString.count, 0)) + hexString.map { String($0) }
-        let red: CGFloat = CGFloat(Int(value[0] + value[1], radix: 16) ?? 0) / 255.0
-        let green: CGFloat = CGFloat(Int(value[2] + value[3], radix: 16) ?? 0) / 255.0
-        let blue: CGFloat = CGFloat(Int(value[4] + value[5], radix: 16) ?? 0) / 255.0
+        let values: [String] = Array(repeating: "0", count: max(6 - hexString.count, 0)) + hexString.map { String($0) }
+        let red: CGFloat = CGFloat(Int(values[0] + values[1], radix: 16) ?? 0) / 255.0
+        let green: CGFloat = CGFloat(Int(values[2] + values[3], radix: 16) ?? 0) / 255.0
+        let blue: CGFloat = CGFloat(Int(values[4] + values[5], radix: 16) ?? 0) / 255.0
         return UIColor.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 
     class func hexString(_ hex: String, alpha: CGFloat = 1.0) -> UIColor {
         let hexString: String = hex.replacingOccurrences(of: "#", with: "")
-        let value: [String] = hexString.map { String($0) } + Array(repeating: "0", count: max(6 - hexString.count, 0))
-        let red: CGFloat = CGFloat(Int(value[0] + value[1], radix: 16) ?? 0) / 255.0
-        let green: CGFloat = CGFloat(Int(value[2] + value[3], radix: 16) ?? 0) / 255.0
-        let blue: CGFloat = CGFloat(Int(value[4] + value[5], radix: 16) ?? 0) / 255.0
+        let values: [String] = hexString.map { String($0) } + Array(repeating: "0", count: max(6 - hexString.count, 0))
+        let red: CGFloat = CGFloat(Int(values[0] + values[1], radix: 16) ?? 0) / 255.0
+        let green: CGFloat = CGFloat(Int(values[2] + values[3], radix: 16) ?? 0) / 255.0
+        let blue: CGFloat = CGFloat(Int(values[4] + values[5], radix: 16) ?? 0) / 255.0
         return UIColor.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 
